fe865cb851d57439eb429c1445e970d91af0d050
barondedios <realbarondedios@gmail.com>
2026-03-31 20:02:42 +0800

    Done playing and updating the 02-vanilla-js-types.js

--- 02-vanilla-js-types.js
+++ 02-vanilla-js-types.js
@@ -1,3 +1,22 @@
-let userName1 = "Elmer";
+// Basic JavaScript Example
 
-console.log(typeof userName1)
+let userName1 = "Elmer";
+let userAge1 = 25;
+let isStudent = true;
+
+console.log("Type of userName1:", typeof userName1);
+console.log("Type of userAge1:", typeof userAge1);
+console.log("Type of isStudent:", typeof isStudent);
+
+// Function
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+
+// Function with math
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(greet(userName1));
+console.log("Product:", multiply(4, 5));
