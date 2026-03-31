327380802eae417ec1b7b3c6a476bbc4de2bc7cf
barondedios <realbarondedios@gmail.com>
2026-03-31 21:08:38 +0800

    Done with 09-special-types.ts

---
+++ 09-special-types.js
@@ -0,0 +1,21 @@
+"use strict";
+// Special Type - null
+let a;
+a = null;
+// a = "Hi"
+let b;
+b = null;
+b = "Hi";
+// b = 10
+console.log("b value:", b);
+// Special Type - undefined
+let c;
+c = undefined;
+// c = null
+// c = "Hi"
+let d;
+d = undefined;
+d = "Hi";
+// d = null
+// d = 10
+console.log("d value:", d);
--- 09-special-types.ts
+++ 09-special-types.ts
@@ -1,25 +1,29 @@
 // Special Type - null
 let a: null;
 
-a = null
+a = null;
 // a = "Hi"
 
-let b: null | string
+let b: null | string;
 
-b = null
-b = "Hi"
+b = null;
+b = "Hi";
 // b = 10
+
+console.log("b value:", b);
 
 // Special Type - undefined
 let c: undefined;
 
-c = undefined
+c = undefined;
 // c = null
 // c = "Hi"
 
-let d: undefined | string
+let d: undefined | string;
 
-d = undefined
-d = "Hi"
+d = undefined;
+d = "Hi";
 // d = null
 // d = 10
+
+console.log("d value:", d);
