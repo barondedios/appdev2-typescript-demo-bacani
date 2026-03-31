f9a58cb9600e19466cf0f79bd3f606daf6b26030
barondedios <realbarondedios@gmail.com>
2026-03-31 20:07:58 +0800

    Done updating and playing with 04-arrays-object.ts

---
+++ 04-arrays-objects.js
@@ -0,0 +1,25 @@
+"use strict";
+let hobbies = ["Eating", "Watching"];
+// hobbies.push(10)
+// let userList: (string | number)[];
+let userList;
+userList = ["Elmer", "Elmer123", 123456];
+// console.log(userList)
+let user = {
+    name: "Elmer",
+    age: 31,
+    hobbies: ["Eating", "Sleeping"],
+    role: {
+        description: 'admin',
+        id: 1
+    }
+};
+// Must not null or undefined
+let val = "Elmer Alvarado";
+// Flexible Object - Record Type
+let data;
+data = {
+    1: 1,
+    name: 'Elmer'
+    // isPartTimeInstructor: true
+};
