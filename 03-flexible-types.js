5dff6a8009c282c90455767179ab9026bf1d4240
barondedios <realbarondedios@gmail.com>
2026-03-31 20:05:10 +0800

    Done updating and playing with 03-flexible-types.ts

---
+++ 03-flexible-types.js
@@ -0,0 +1,19 @@
+"use strict";
+let age = 31;
+// Change values
+age = "31";
+age = true;
+age = {};
+age = [];
+// Function to describe the type
+function describeValue(value) {
+    return `Value: ${JSON.stringify(value)} | Type: ${typeof value}`;
+}
+// Log different examples
+console.log(describeValue(31));
+console.log(describeValue("hello"));
+console.log(describeValue(true));
+console.log(describeValue({ name: "Elmer" }));
+console.log(describeValue([1, 2, 3]));
+// Final value of age
+console.log("Final age variable:", describeValue(age));
